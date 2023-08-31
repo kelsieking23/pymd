3911e2178aca73c3f500f8fe6088bbfae795db3d
Kelsie King <kelsieking23@vt.edu>
2023-08-31 09:19:23 -0400

    more hbond bug fixes

--- mdanalysis/hbond.py
+++ mdanalysis/hbond.py
@@ -13,6 +13,7 @@ from collections.abc import Iterable
 from sklearn.preprocessing import StandardScaler
 from pymd.mdanalysis.analysis import Analysis
 from pymd.structure.peptide import Peptide
+import multiprocessing as mp
 
 
 class Hbond(Analysis):
@@ -45,7 +46,20 @@ class Hbond(Analysis):
         self.verbose = False
         self.__dict__.update(kwargs)
 
-    def run(self, selection, freq_step=0.01, method='baker-hubbard', output='hbond.csv', **kwargs):
+    def starmap_wrapper(self, arg):
+        args, kwargs = arg
+        return self.runBakerHubbard(*args, **kwargs)
+    
+    def launch_mp_bakerHubbard(self, processes, freq_step, freq_max, selection, **kwargs):
+        jobs = []
+        pool = mp.Pool(processes)
+        for i in np.arange(freq_step, freq_max, freq_step):
+            jobs.append((i, selection, i+i))
+        arg = [(j, kwargs) for j in jobs]
+        results = pool.map(self.starmap_wrapper, arg)
+        return results
+    
+    def run(self, selection, freq_step=0.01, freq_max=1, method='baker-hubbard', output='hbond.csv', processes=1, **kwargs):
         self._output = output
         try:
             assert self.traj is not None
@@ -66,12 +80,12 @@ class Hbond(Analysis):
         if self.verbose:
             print('Beggining hbond calculation...')
         if (method == 'baker-hubbard'):
-            self.df = self.runBakerHubbard(freq_step, selection, **kwargs)
+            self.df = self.runBakerHubbard(freq_step, selection, freq_max=freq_max, **kwargs)
         else:
             raise ValueError('No method called {}. Only supported method currently is baker-hubbard'.format(method))
         if self.verbose:
             print('Hbond calculation complete.')
-        self.df.to_csv(self.output)
+        # self.df.to_csv(self.output)
         
     def getBlankMatrix(self, top_atoms):
         m = {}
@@ -90,7 +104,7 @@ class Hbond(Analysis):
                 m[res_id][res] = 0 
         return m
     
-    def runBakerHubbard(self, freq_step, selection, **kwargs):
+    def runBakerHubbard(self, freq_step, selection, freq_max, **kwargs):
         if isinstance(selection, str):
             top_atoms = self.select(selection)
         elif isinstance(selection, (list, tuple, np.ndarray,)):
@@ -99,7 +113,7 @@ class Hbond(Analysis):
             raise ValueError('Selection must be a string (mdtraj selection string) or array-like. Selection was {}'.format(selection))
         hbond_data = []
         base, ext = tuple(os.path.splitext(self.output))
-        freqs = np.arange(freq_step, 1+freq_step, freq_step)
+        freqs = np.arange(freq_step, freq_max, freq_step)
         percents = np.arange(0.1, 1.1, 0.1)
         for i, freq in enumerate(freqs):
             if freq > 1:
@@ -132,7 +146,7 @@ class Hbond(Analysis):
         return maxed
 
 
-
+    
     def bakerHubbard(self, freq, **kwargs):
         hbonds = mdtraj.baker_hubbard(self._traj, freq=freq, **kwargs)
         return hbonds
